b320d220327e6e5394984ca86d6e090bfe579073
Tandith 0005 <tandith0005@gmail.com>
2026-02-03 15:37:39 +0600

    added app/page.tsx

--- src/app/(CommonLayout)/layout.tsx
+++ src/app/(CommonLayout)/layout.tsx
@@ -1,13 +1,11 @@
-import Footer from '@/components/layout/Footer';
-import Navbar from '@/components/layout/Navbar';
 import React from 'react';
 
 const CommonLayout = ({children}: {children: React.ReactNode}) => {
     return (
         <div>
-            <Navbar />
+   
             {children}
-            <Footer />
+   
         </div>
     );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Toaster from "./lib/Toaster";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +31,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        <Toaster>{children}</Toaster>
+        <Toaster><Navbar />
+            {children}
+            <Footer /></Toaster>
       </body>
     </html>
   );
