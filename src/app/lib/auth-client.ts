e88a62da721c2becbf41e81a01a84ede7fbc6abc
Tandith 0005 <tandith0005@gmail.com>
2026-02-06 16:24:42 +0600

    cors issue

--- src/app/lib/auth-client.ts
+++ src/app/lib/auth-client.ts
@@ -1,4 +1,11 @@
 import { createAuthClient } from "better-auth/react"
+// export const authClient = createAuthClient({
+//     baseURL: `${process.env.NEXT_PUBLIC_API_URL}`
+// })
+
 export const authClient = createAuthClient({
-    baseURL: `${process.env.NEXT_PUBLIC_API_URL}`
-})
+  baseURL: typeof window !== "undefined" ? window.location.origin : "",
+  fetchOptions: {
+    credentials: "include",
+  },
+});
